cd4bc0ad0e22bfaafb639d37c59e283bd1912151
kagan <kagan_43@mail.ru>
2025-05-31 19:06:33 +0700

    теперь это правдв скрипт

--- DB/make_backup.sh
+++ DB/make_backup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo docker exec -ti db_postgres /bin/bash
-
-pg_dump --file "/home/backup.sql" --username "root" --no-password --format=p --encoding "UTF8" --inserts --column-inserts --verbose "root"
-
-exit
+sudo docker exec -ti db_postgres /bin/bash -c 'pg_dump --file "/home/backup.sql" --username "root" --no-password --format=p --encoding "UTF8" --inserts --column-inserts --verbose "root"'
 
 sudo cat /var/lib/docker/volumes/kr_db_volume/_data/backup.sql > backup.sql
+
+echo "Backup completed and saved to backup.sql"
